0844883aad23e1f7c04095541e650040a58e7642
Anthony Lamping <lamping.ap@gmail.com>
2018-04-02 21:28:41 -0400

    add docker images

---
+++ vars/dockerImages.groovy
@@ -0,0 +1,25 @@
+// Docker images
+
+def getArch(){
+    return "px4io/px4-dev-base-archlinux:2018-03-30"
+}
+
+def getARMhf(){
+    return "px4io/px4-dev-armhf:2017-12-30"
+}
+
+def getBase(){
+    return "px4io/px4-dev-base:2018-03-30"
+}
+
+def getNuttx(){
+    return "px4io/px4-dev-nuttx:2018-03-30"
+}
+
+def getROS(){
+    return "px4io/px4-dev-ros:2018-03-30"
+}
+
+def getRPi(){
+    return "px4io/px4-dev-raspi:2018-03-30"
+}
